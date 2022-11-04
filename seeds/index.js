a27b32443bbcd6c31aeb6567aeb688f8dd06e60b
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-11-04 20:35:11 +0700

    Styling Campgrounds index

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seeder = async () => {
   await Campground.deleteMany({})
   for(let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 1
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: `https://source.unsplash.com/collection/483251`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea soluta expedita veniam iste nemo! Cum!",
+      price
     })
     await camp.save()
   }
