618329b51b3ebf11c024f69a537849ff694d300b
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-11-06 15:48:08 +0700

    Basic Errors Handler

--- app.js
+++ app.js
@@ -38,10 +38,14 @@ app.get('/campgrounds/new', (req ,  res) => {
   res.render('campgrounds/new')
 })
 
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground)
-  await campground.save()
-  res.redirect(`/campgrounds/${campground._id}`)
+app.post("/campgrounds", async (req, res, next) => {
+  try {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+  } catch (e) {
+    next(e)
+  }
 })
 
 app.get("/campgrounds/:id", async (req,res) => {
@@ -65,6 +69,14 @@ app.delete('/campgrounds/:id', async (req,res) => {
   await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
 })
+
+app.use((err, req, res, next) => {
+  res.send('OH BOY SOMETHING WENT WRONG')
+})
+
+
+
+
 
 
 app.listen(3000, () => {
