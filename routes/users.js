4dcebe4de8dffafe948b55b4f542b884cebd601b
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-11-13 18:39:12 +0700

    adding logout

--- routes/users.js
+++ routes/users.js
@@ -31,5 +31,14 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
   res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/login');
+  });
+})
 
 module.exports = router
