f7e00338d1f1567171838537c3c2ef2b8c772e37
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-11-13 16:29:41 +0700

    configure passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -43,10 +46,24 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next()
+})
+
+
+app.get('/fakeuser', async (req, res) => {
+    const user = new User({email: "admin@admin.com", username: "admin"})
+    const newUser = await User.register(user, 'admin')
+    res.send(newUser)
 })
 
 app.get("/", (req, res) => {
