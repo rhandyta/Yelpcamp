609bf441aa71c6aa59297688e110d620b9411351
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-09 03:36:09 +0700

    pc

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const sessionConfig = {
     cookie: {
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        // secure: true,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     },
 };
